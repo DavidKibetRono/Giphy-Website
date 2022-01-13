fde8674a9d1b5a037a4e8c39aae1f06ea172d576
DavidKibetRono <david.rono@student.moringaschool.com>
2022-01-13 16:51:46 +0300

    add the routing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DisplayGifsComponent } from './display-gifs/display-gifs.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {SpecificGifComponent} from "./specific-gif/specific-gif.component";
+import {RegisterComponent} from "./register/register.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home",component:HomeComponent},
+  {path:"login",component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:"gifs",component:DisplayGifsComponent},
+  {path:"gifs/:id",component:SpecificGifComponent},
+  {path:"",redirectTo:"home",pathMatch:'full'},
+  {path:"**",component:PageNotFoundComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
